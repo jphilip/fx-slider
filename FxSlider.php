4b5c06997decf7bfdac3b9f3336014637101877b
Jacques Philip <jphilip@alaska.edu>
2021-03-26 11:51:21 -0800

    Initial commit

---
+++ FxSlider.php
@@ -0,0 +1,121 @@
+<?php
+namespace CANHR\FxSlider;
+
+use \REDCap as REDCap;
+require_once "emLoggerTrait.php";
+
+class FxSlider extends \ExternalModules\AbstractExternalModule
+{
+  use emLoggerTrait;
+
+  private $pid;
+  private $timer_js_file;
+  private $range_js_file;
+  private $slider_js_file;
+  private $slider_css_file;
+  private $show_textbox;
+  private $show_score;
+  private $show_slider_msg;
+  private $slider_msg;
+  
+  
+  function __construct()
+  {
+    parent::__construct();
+    $this->pid = $this->getProjectId();
+  }
+    
+  function redcap_survey_page_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL,
+  $survey_hash = NULL, $response_id = NULL, $repeat_instance = 1)
+  {
+    
+    $this->timer_js_file = $this->getUrl('/assets/timer.js');
+    $this->range_js_file = $this->getUrl('/assets/range.js');
+    $this->slider_js_file = $this->getUrl('/assets/slider.js');
+    $this->slider_css_file = $this->getProjectSetting("css-url");
+    if (empty($this->slider_css_file))
+      $this->slider_css_file = $this->getUrl('/assets/bluecurve.css');
+    $this->show_textbox = (int) $this->getProjectSetting("show-textbox");
+    if (empty($this->show_textbox))
+      $this->show_textbox = 0;
+    $this->show_score = (int) $this->getProjectSetting("show-score");
+    if (empty($this->show_score) | !$this->show_textbox)
+      $this->show_score = 0;
+    $this->show_slider_msg = (int) $this->getProjectSetting("show-slider-msg");
+    if (empty($this->show_slider_msg))
+      $this->show_slider_msg = 0;
+    $this->slider_msg = $this->getProjectSetting("slider-msg");
+    if (empty($this->slider_msg))
+      $this->slider_msg = "Move the slider out of the left box to set a response";
+
+    $str = <<<"EOT"
+
+<script type="text/javascript" src="{$this->timer_js_file}"></script>
+<script type="text/javascript" src="{$this->range_js_file}"></script>
+<script type="text/javascript" src="{$this->slider_js_file}"></script>
+<link rel="stylesheet" type="text/css" media="all" href="{$this->slider_css_file}">
+
+<script type="text/javascript">
+$(document).ready( function(){
+  $(".sliderWrapper").each(function(i ,wp) {
+    var qId = wp.id; //Name of REDCap variable and control, id of slider wrapper 
+    var redcapInput = document.getElementsByName(qId)[0];
+    redcapInput.style.width = "40px";
+    redcapInput.style.marginTop = "15px";
+    
+  ////// If module setting show-textbox is true, show textbox too///////////  
+  ///// This was a hack for hook framework needs to be changed to an option for EM /////
+    if (!{$this->show_textbox})
+      redcapInput.style.display = 'none';
+    
+    var el = document.getElementById(qId + "-anchors");
+    if (el)
+      var anchors = el.value.split("|");
+    else
+      var anchors = ["Not at all", "Somewhat", "A lot"];
+        
+    // Set height of slider wrapper
+    wp.style.height = "50px";
+    if ({$this->show_slider_msg})
+      wp.style.height = "85px";
+
+    wp.innerHTML = '<table style="width: 650px"><tbody><tr class="ticks"><td style="width:  47px;"></td><td style="width: 144px;">|</td><td style="width: 144px;">|</td><td style="width: 144px;">|</td><td style="width: 144px;">|</td><td>|</td></tr></tbody></table><div id="slider-' + qId + '" class="horizontal dynamic-slider-control slider" style="left: 1px; width: 650px; position: relative; top: 0px; height: 25px;"><input id="slider-input-' + qId + '" class="slider-input" style="width: 465px; position: relative; height: 16px"><div>&nbsp;</div><table style="width: 650px; position: relative; left: 0px; top: 8px;"><tbody><tr class="scaleRow"><td style="width: 106px;">' + anchors[0] + '</td><td style="width: 109px;">&nbsp;</td><td>' + anchors[1] + '</td><td style="width: 103px;">&nbsp;</td><td style="width:  94px;text-align:right;padding-right:5px;">' + anchors[2] + '</td></tr><tr><td id="slidermsg-' + qId + '" class="sldrmsg opacity75" colspan="6">{$this->slider_msg}</td></tr></tbody></table>';
+        
+    var s1 = new Slider(document.getElementById("slider-" + qId), document.getElementById("slider-input-" + qId), 520, 0);
+    
+    if (redcapInput.value == '')
+      redcapInput.value = 0;
+    s1.setValue(redcapInput.value);
+  
+    s1.onchange = function () {
+      fishValue = s1.getValue();
+      if (fishValue < 20) {
+        fishScore = 0;
+        if ($this->show_slider_msg)
+          $("#slidermsg-" + qId).show();
+      } else {
+        fishScore = Math.ceil((fishValue-20)/(506-20)*19);
+        $("#slidermsg-" + qId).hide();
+      }
+  
+  ///////Show score to test, but value (slider position) in production)////////
+      redcapInput.value = !{$this->show_score} ? fishValue : fishScore;
+    };
+
+    // Hide slider message at first if set up that way
+    if (!{$this->show_slider_msg})
+      $("#slidermsg-" + qId).hide();
+
+  });
+});
+</script>
+EOT;
+    echo $str; 
+  }
+
+  function redcap_data_entry_form_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $repeat_instance = 1)
+  {
+      $this->redcap_survey_page_top($project_id, $record, $instrument, $event_id, $group_id,
+      NULL, NULL, $repeat_instance);
+  }
+}
---
+++ config.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Include REDCap header.
+ */
+require_once APP_PATH_DOCROOT . "ProjectGeneral/header.php"; 
+
+
+
+/**
+ * Initialize Custom Report Builder object, and call method
+ * to generate Index page.
+ */
+//$customReportBuilder = new \BCCHR\CustomTemplateEngine\CustomTemplateEngine();
+//$customReportBuilder->generateIndexPage();
+?>
+
+<h4>Fx Slider Configuration</h4>
+<h5>Enable Fx Slider for a field</h5>
+In the designer:
+<ul>
+    <li>Set the field type to Text Box</li>
+    <li>Set the custom alignment to Left / Vertical (LV)</li>
+    <li>In the field label set the label as usual, but add a div HTML element like this:<br>
+        <code>&lt;div class=&quot;sliderWrapper&quot; id=&quot;field_variable_name&quot;&gt;&lt;/div&gt;</code><br>
+        replacing field_variable_name with the actual variable name of the field in question.<br>
+        For example for a filed having a variable name of help_fish, you would use:<br>
+        <code>How do you feel this helped?<br>
+        &lt;div class=&quot;sliderWrapper&quot; id=&quot;help_fish&quot;&gt;&lt;/div&gt;</code>
+    </li>
+</ul>
+
+<h5>Changing the anchor labels</h5>
+<ul>
+    <li>By default the slider anchors will be: Not at all , Somewhat , A lot</li>
+    <li>To change them, add a hidden HTML element like this:<br>
+        <code>&lt;input type=&quot;hidden&quot; id=&quot;field_variable_name-anchors&quot; value=&quot;Low anchor|Middle anchor|High anchor&quot;&gt;</code><br>
+        replacing field_variable_name with the actual variable name of the field in question.<br>
+        Adding to the previous example for a variable name of help_fish, you would use:<br>
+        <code>How do you feel this helped?<br>
+        &lt;div class=&quot;sliderWrapper&quot; id=&quot;help_fish&quot;&gt;&lt;/div&gt;&lt;input type=&quot;hidden&quot; id=&quot;help_fish-anchors&quot; value=&quot;Very little|Some|Very much&quot;&gt;</code>
+    </li>
+</ul>
+
+<h5>Other settings</h5>
+If you go to to External Modules in the Applications left menu and click on Configure Fx Slider Module, you can change
+the following settings:
+<ul>
+    <li>Show the slider's no answer message:<br>
+        When checked the no answer message is visible when the slider is in the no answer zone.
+    </li>
+    <li>Slider's no answer message:<br>
+        Message displayed when when the slider is in the no answer zone.<br>
+        Defaults to "Move the slider out of the left box to set a response".
+    </li>
+    <li>Url for custom css file (e.g., to customize handle and background): (Admin only)<br>
+        This would be the URL of a css file on the server which would reference custom graphics, colors... to change the
+        slider's aspect.
+    </li>
+    <li>Show the underlying textbox for testing:<br>
+        This shows the text box whose value REDCap actually saves (by default the position of the slider handle).<br>
+        This allows you to verify that the correct value changes as you move the handle when you have multiple sliders
+        on a page.
+    </li>
+    <li>Show the scores rather than the position of the slider in the textbox: (Admin only.<span style="color:red;"> DO NOT USE IN PRODUCTION</span>)<br>
+        This displays the score on a 0 to 20 scale (0 being no answer) instead of the slider position.<br>
+        This allows to visualize where the scores fall relative to the anchors (for development).<br>
+        Be aware that if you save the record when the score is displayed, it will be saved a if it where a position,
+        effectively removing the response.<br>
+        The score is calculated as:<br>
+        <code>if (position < 20) score = 0 else score = Math.ceil((position-20)/(506-20)*19)</code>
+    </li>
+</ul>
+
+<?php
+/**
+ * Include REDCap footer.
+ */
+require_once APP_PATH_DOCROOT . "ProjectGeneral/footer.php";
+?>
